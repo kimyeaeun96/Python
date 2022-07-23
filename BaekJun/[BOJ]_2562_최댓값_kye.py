9dc0e2c5b639f3f4e1f267301de93780f9fcb8b6
kimyeaeun96 <108277788+kimyeaeun96@users.noreply.github.com>
2022-07-23 22:19:38 +0900

    [BOJ]_2562_최댓값_kye

---
+++ BaekJun/[BOJ]_2562_최댓값_kye.py
@@ -0,0 +1,13 @@
+#9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고
+#최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+#첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
+
+num = []
+
+for i in range(0, 9) :
+    user_input = int(input("자연수를 입력하세요."))
+    num.append(user_input)
+
+print(int(max(num)))
+print(num.index(max(num))+1)
+    
